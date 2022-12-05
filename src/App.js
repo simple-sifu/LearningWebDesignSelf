9887aaf72beeb75c59e156a62d1cdeeded9d29c0
Tommy Han <36891099+simple-sifu@users.noreply.github.com>
2022-12-05 15:27:09 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div id="container">
+      <div class="box box1">1</div>
+      <div class="box box2">2</div>
+      <div class="box box3">3</div>
+      <div class="box box4">4</div>
+      <div class="box box5">5</div>
+    </div>
+  );
+}
